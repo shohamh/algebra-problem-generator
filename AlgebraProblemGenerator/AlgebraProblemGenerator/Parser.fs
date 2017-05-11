f02a5acc3ff550909079c6c10ca4e5f2be5a17f6
shohamh <shohamh@hotmail.com>
2017-05-11 08:48:26 -0700

    Added some parsers relevant to MathML, getting close to know what I'm doing, not there yet. Also, it doesn't compile cause I suck.

--- AlgebraProblemGenerator/AlgebraProblemGenerator/Parser.fs
+++ AlgebraProblemGenerator/AlgebraProblemGenerator/Parser.fs
@@ -10,17 +10,50 @@ let test p str =
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
-let ws = spaces
 //let pstr = pstring
-let pstr s = ws >>. pstringCI s .>> ws
-//let sstr = skipStringCI .>> ws
+let pstr s = spaces >>. pstringCI s .>> spaces
+let sstr s = spaces >>. skipStringCI s .>> spaces 
 
-//let skipmathMLTag s = sstr "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">" .>> preturn s >>. sstr "</math>"
+
+type Mthings =
+| Identifier of string
+| Operator of string
+| Float of float
+
+// BTag = Begin Tag
+// ETag = End Tag
+// ITag = the insides between a BTag and a ETag
+
+// returns the attributes of the tag:
+let pBTag tagname = between (sstr ("<" + tagname)) (sstr ">") (manyChars (noneOf ">")) //TODO: watch out for \> in before the end of the tag (>)
+
+let pETag tagname = sstr ("</" + tagname + ">")
+
+
+let pMi = between (pBTag "mi") (pETag "mi") (Mthings.Identifier (many1 anyChar))
+let pMo = between (pBTag "mo") (pETag "mo") (Mthings.Operator (pstr "+" <|> pstr "-"))
+let pMn = between (pBTag "mn") (pETag "mn") (Mthings.Float pfloat)
+let pMrow = between (pBTag "mrow") (pETag "mrow") (many <| choice [pMi; pMo; pMn])
+let pMstyle p = between (pBTag "mstyle") (pETag "mstyle") p
+let pMath p = between (pBTag "math") (pETag "math") p
+
+let pMainTags = [pMrow; pMi; pMo; pMn]
+
+let pMathML = pMath (pMstyle (many <| choice (pMainTags )))
+
+
 
 
 let tests = 
     //test (skipmathMLTag "hi" mathmltest)
+    printfn "Parser tests:"
+    printfn "----------------------------\n"
     let parseVariable = pstr "x"
     test parseVariable " X "
-    //test pfloat "hi"
-    //test pchar 3
+    let skipVariable = sstr "x"
+    test skipVariable " X "
+    test pMathML "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n  <mstyle displaystyle=\"true\">\n    <mi> r </mi><mo>+</mo><mn>3</mn>\n  </mstyle>\n</math>"
+    test (pBTag "mstyle") "<mstyle displaystyle=\"true\">"
+    test (pETag "mstyle") "</mstyle>"
+
+    printfn "\n----------------------------\n\n"
