fdc7e7f6e432a3aa8074e7784be27c050670d23d
shohamh <shohamh@hotmail.com>
2017-06-12 12:03:02 -0700

    Nicer formatting for the parsers

--- AlgebraProblemGenerator/AlgebraProblemGenerator/Parser.fs
+++ AlgebraProblemGenerator/AlgebraProblemGenerator/Parser.fs
@@ -1,7 +1,6 @@
 ﻿module Parser
 
 open FParsec
-open AlgebraProblemGenerator
 
 let mathmltest = "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n  <mstyle displaystyle=\"true\">\n    <mi> r </mi>\n  </mstyle>\n</math>"
 
@@ -10,14 +9,18 @@ let test p str =
     | Success(result, _, _)   -> printfn "Test\n==============\n Success: %A\n==============\n" result
     | Failure(errorMsg, _, _) -> printfn "Test\n==============\n Failure: %A\n==============\n" errorMsg
 
+let debug = false
 
 // Debug trace thing
 let (<!>) (p: Parser<'a, 'b>) label =
-    fun (stream : CharStream<'b>) ->
-        printfn "%A: Entering %s with input: %s" stream.Position label (stream.PeekString 1000) 
-        let reply = p stream
-        printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
-        reply
+    if debug then
+        fun (stream : CharStream<'b>) ->
+            printfn "%A: Entering %s with input: %s" stream.Position label (stream.PeekString 1000) 
+            let reply = p stream
+            printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+            reply
+    else
+        p
 
 //let pstr = pstring
 let pstr s = spaces >>. pstringCI s .>> spaces
@@ -37,26 +40,28 @@ type Mtag =
 // ITag = the insides between a BTag and a ETag
 
 // returns the attributes of the tag:
-let pBTag tagName = between (sstr ("<" + tagName)) (sstr ">") (manyTill (noneOf ">") (lookAhead (sstr ">"))) <!> "pBTag" //TODO: watch out for '\>' before the actual end of the tag (>)
-let pETag tagName = between (sstr "</") (sstr ">") (pstr tagName) <!> "pETag"
+let pBTag tagName  = between (sstr ("<" + tagName)) (sstr ">") (manyTill (noneOf ">") (lookAhead (sstr ">"))) <!> "pBTag" //TODO: watch out for '\>' before the actual end of the tag (>)
+let pETag tagName  = between (sstr "</") (sstr ">") (pstr tagName) <!> "pETag"
 let pTag tagName p = between (pBTag tagName) (pETag tagName) p <!> "pTag"
 
-let ident = identifier(IdentifierOptions())
+let identifier = identifier(IdentifierOptions())
 
-let pMi = (pTag "mi" (ident |>> Mtag.Identifier)) <!> "pMi"
-let pMo = (pTag "mo" ((pstr "+" <|> pstr "-") |>> Mtag.Operator)) <!> "pMo"
-let pMn = (pTag "mn" ((pfloat) |>> Mtag.Number)) <!> "pMn"
-let pMrow = (pTag "mrow" ((many <| choice [pMi; pMo; pMn]) |>> Mtag.Row)) <!> "pMrow"
-let pMstyle p = (pTag "mstyle" p) <!> "pMstyle"
-let pMathTag p = (pTag "math" p) <!> "pMathTag"
+let pMi        = pTag "mi" (identifier |>> Mtag.Identifier) <!> "pMi"
+let pMo        = pTag "mo" (pstr "+" <|> pstr "-" |>> Mtag.Operator) <!> "pMo"
+let pMn        = pTag "mn" (pfloat |>> Mtag.Number) <!> "pMn"
 
-let pMainTags = [pMrow; pMo; pMi; pMn]
-let pMathML = pMathTag (pMstyle (many <| choice pMainTags)) <!> "pMathML"
+let pMrowTags  = [pMi; pMo; pMn]
+
+let pMrow      = pTag "mrow" (many <| choice pMrowTags |>> Mtag.Row) <!> "pMrow"
+let pMstyle  p = pTag "mstyle" p <!> "pMstyle"
+let pMathTag p = pTag "math" p <!> "pMathTag"
+
+let pMainTags  = [pMrow; pMo; pMi; pMn]
+let pMathML    = pMathTag (pMstyle (many <| choice pMainTags)) <!> "pMathML"
 
 
 let tests = 
     printfn "Parser tests:"
     printfn "----------------------------\n"
-    test pMo "<mo>+</mo>"
     test pMathML "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n  <mstyle displaystyle=\"true\">\n  <mrow>  <mi> r </mi><mo>+</mo><mn>3</mn>\n </mrow> </mstyle>\n</math>"
     printfn "\n----------------------------\n\n"
