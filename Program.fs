e91325473bbc21200e6f1a010f171f47336b3392
shohamh <shohamh@hotmail.com>
2017-11-23 14:23:58 +0000

    added argument parser and checkequality function

--- Program.fs
+++ Program.fs
@@ -7,6 +7,7 @@ open FParsec
 open QueryBuilder
 open QueryExecutor
 open Utils
+open Argu
 
 let mathMLStrings = [
     "<math xmlns=\"http://www.w3.org/1998/Math/MathML\">\n  <mstyle displaystyle=\"true\">\n  <mrow>  <mn>2</mn><mi>z</mi><mi> r </mi><mo>-</mo><mn>3</mn><mo>-</mo><mi>b</mi><mo>/</mo><mi>b</mi><mo>/</mo><mi>b</mi><mo>-</mo><mi>b</mi>\n </mrow> </mstyle>\n</math>"; 
@@ -40,24 +41,49 @@ let generateSimilarMathMLFromMathML (mathML : string) : string =
     | None ->
         ""
 
+let checkEquality (mathml1: string) (mathml2: string) : bool =
+    let term1 = term mathml1
+    printfn "term1: %A" term1
+    let term2 = term mathml2
+    printfn "term2: %A" term2
+
+    term1 = term2
+
+type CLIArguments =
+    | CheckEquality of mathml1:string * mathml2:string
+    | GenerateSimilarTerm of mathml:string
+    | Debug
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | CheckEquality _ -> "Checks for equality between two mathML strings."
+            | GenerateSimilarTerm _ -> "Generates a similar mathML term."
+            | Debug -> "Prints debug messages for parsers."
 
 
 [<EntryPoint>]
 let main argv =
+    let parser = ArgumentParser.Create<CLIArguments>(programName = "dotnet")
+    let tempres = parser.Parse argv
+    let parserResults =  tempres.GetAllResults()
+    //printfn "%A" parserResults
+    let rec parseCommandline parserResults =
+        match parserResults with
+        | [] -> 
+            printf ""
+        | x::xs ->
+            match x with
+            | CheckEquality (mathml1, mathml2) ->
+                printfn "%A" (checkEquality mathml1 mathml2)
+            | GenerateSimilarTerm mathml ->
+                printfn "%s" (generateSimilarMathMLFromMathML mathml)
+            parseCommandline xs
+    parseCommandline parserResults
     // printfn "hi"
     // printfn "%A" Parser.tests 
     // printfn "bye"
     // let testedTerms : Term option list = List.map generateSimilarTermFromMathML mathMLStrings
-    if not (List.isEmpty (Array.toList argv)) then
-        let argument = List.item 0 <| Array.toList argv
-        if argument = "--debug" then
-            Parser.debug <- true
-            let argument = List.item 1 <| Array.toList argv
-            printfn "%s" (generateSimilarMathMLFromMathML argument)
-        else
-            printfn "%s" (generateSimilarMathMLFromMathML argument)
-    else
-        printfn "add MathML argument in argv"
     // let requestedTerms = 
     //     if List.length (Array.toList argv) > 0 then
     //         List.map generateSimilarMathMLFromMathML (Array.toList argv)
